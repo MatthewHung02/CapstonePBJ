6131515ead64b438bade9499bb71163914ab3fcf
Marcus <55485519+lazarom37@users.noreply.github.com>
2024-05-10 02:43:39 -0700

    saving progress, not finished. Managed to make scene switching

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -19,11 +19,14 @@ public class GameMenuManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(showButton.action.WasPressedThisFrame())
+        if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive(!menu.activeSelf);
 
             menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
         }
+        //Makes menu look at viewer's head
+        menu.transform.LookAt(new Vector3 (head.position.x, menu.transform.position.y, head.position.z)); //This doesn't work, but I'm keeping it here for reference
+        menu.transform.forward *= -1;   //The menu will show up inversed without this
     }
 }
